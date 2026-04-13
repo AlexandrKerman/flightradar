a0f355f322516547ad83fba95a1699319596f2ed
Kerbin Original <aakolomeycev@gmail.com>
2026-04-13 15:48:52 +0300

    docstrings in classes.py

--- src/classes.py
+++ src/classes.py
@@ -23,11 +23,17 @@ class AeroplanesAPI(bc.BaseAPI):
     __token = None
 
     def __init__(self):
+        '''
+        Инициализатор объекта
+        '''
         self.__aeroplanes = []
         self.__box = {}
         # self.__token = None
 
     async def __get_token(self) -> None:
+        '''
+        Получение токена для работы с API. Приватный.
+        '''
         async with aiohttp.ClientSession() as session:
             data = {
                 "grant_type": "client_credentials",
@@ -40,6 +46,11 @@ class AeroplanesAPI(bc.BaseAPI):
 
     @staticmethod
     def retry_connection(limit: int = 5):
+        '''
+        Декоратор для повторной попытки подключения.
+        Вызывает декорируемую функцию limit раз (по умолчанию: 5),
+        если http-статус не 200.
+        '''
         def wrapper(func):
             @wraps(func)
             async def inner(*args, **kwargs) -> aiohttp.ClientResponse | None:
@@ -56,6 +67,9 @@ class AeroplanesAPI(bc.BaseAPI):
         return wrapper
 
     async def __get_request(self, url, headers=None, params=None) -> Optional[Any]:
+        '''
+        Получение и парсинг ответа с api в json-формат. Приватный
+        '''
         response = await self.__connect(url, headers, params)
         if response:
             return await response.json()
@@ -64,12 +78,18 @@ class AeroplanesAPI(bc.BaseAPI):
     @staticmethod
     @retry_connection()
     async def __connect(url: str, headers: dict | None = None, params: dict | None = None) -> aiohttp.ClientResponse:
+        '''
+        Выполняет подключение по api по указанному url с headers и params. Приватный
+        '''
         async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, headers=headers) as response:
                 await response.read()
                 return response
 
     async def set_box(self, country: str) -> None:
+        '''
+        Получение bounging_box страны
+        '''
         url = f"{self.__openstreet_url}/search"
         header = {"User-Agent": "aeroplanes-app"}
         payload = {"country": country, "format": "json", "limit": 1}
@@ -78,6 +98,9 @@ class AeroplanesAPI(bc.BaseAPI):
             self.__box = {"lamin": box[0], "lamax": box[1], "lomin": box[2], "lomax": box[3]}
 
     async def get_aeroplanes(self) -> None:
+        '''
+        Получение списка словарей самолётов в bounding_box.
+        '''
         url = f"{self.__opensky_url}/states/all"
         if not self.__token:
             await self.__get_token()
@@ -115,6 +138,9 @@ class AeroplanesAPI(bc.BaseAPI):
 
     @property
     def aeroplanes(self) -> list:
+        '''
+        Возвращает полную копию списка самолётов. Свойство
+        '''
         return deepcopy(self.__aeroplanes)
 
 
@@ -140,31 +166,49 @@ class Aeroplane(bc.BaseAeroplane):
     )
 
     def __init__(self, **kwargs):
+        '''
+        Инициализатор. Принимает n именных аргументов самолёта.
+        '''
         if kwargs:
             for attr in self.__slots__:
                 setattr(self, attr, kwargs.get(attr))
 
     def __iter__(self):
+        '''
+        Инициализация итератора
+        '''
         self.index = -1
         return self
 
     def __next__(self):
+        '''
+        Возвращает след. атрибут объекта
+        '''
         self.index += 1
         if self.index < len(self.__slots__):
             return self.__slots__[self.index], self.__getattribute__(self.__slots__[self.index])
         raise StopIteration
 
     def __comparison_tuple(self):
+        '''
+        Возвращает кортеж для сравнения. Приватный
+        '''
         if all((self.baro_altitude, self.velocity)):
             return self.baro_altitude, self.velocity
         return self.baro_altitude if self.baro_altitude else 0, self.velocity if self.velocity else 0
 
     def __lt__(self, other) -> bool:
+        '''
+        Сравнение по кортежу сравнения по оператору <
+        '''
         if isinstance(other, Aeroplane):
             return self.__comparison_tuple() < other.__comparison_tuple()
         raise TypeError(f"Expected Aeroplane object. Got {type(other)}")
 
     def __le__(self, other) -> bool:
+        '''
+        Сравнение по кортежу сравнения по оператору <=
+        '''
         if isinstance(other, Aeroplane):
 
             if all((self.baro_altitude, self.velocity, other.baro_altitude, other.velocity)):
@@ -173,6 +217,9 @@ class Aeroplane(bc.BaseAeroplane):
         raise TypeError(f"Expected Aeroplane object. Got {type(other)}")
 
     def __gt__(self, other) -> bool:
+        '''
+        Сравнение по кортежу сравнения по оператору >
+        '''
         if isinstance(other, Aeroplane):
 
             if all((self.baro_altitude, self.velocity, other.baro_altitude, other.velocity)):
@@ -181,6 +228,9 @@ class Aeroplane(bc.BaseAeroplane):
         raise TypeError(f"Expected Aeroplane object. Got {type(other)}")
 
     def __ge__(self, other) -> bool:
+        '''
+        Сравнение по кортежу сравнения по оператору >=
+        '''
         if isinstance(other, Aeroplane):
 
             if all((self.baro_altitude, self.velocity, other.baro_altitude, other.velocity)):
@@ -189,6 +239,9 @@ class Aeroplane(bc.BaseAeroplane):
         raise TypeError(f"Expected Aeroplane object. Got {type(other)}")
 
     def __eq__(self, other) -> bool:
+        '''
+        Сравнение по кортежу сравнения по оператору =
+        '''
         if isinstance(other, Aeroplane):
 
             if all((self.baro_altitude, self.velocity, other.baro_altitude, other.velocity)):
@@ -197,18 +250,34 @@ class Aeroplane(bc.BaseAeroplane):
         raise TypeError(f"Expected Aeroplane object. Got {type(other)}")
 
     def __repr__(self) -> str:
+        '''
+        Отоадочное представление объекта
+        '''
         return f"Aeroplane({', '.join([f'{i}={self.__getattribute__(i)}' for i in self.__slots__])})"
 
     def __str__(self) -> str:
+        '''
+        Строковое представление объекта в виде:
+        <Callsign> (<ICAO24>) in <country> have velocity <velocity> and flies in <baro_altitude> above the sea level
+        '''
         return (
             f"{self.Callsign} ({self.ICAO24}) in {self.country} have velocity {self.velocity} "
             f"and flies in {self.baro_altitude if self.baro_altitude else 0} above the sea level."
         )
 
     def __bool__(self) -> bool:
+        '''
+        Булевое представление объекта.
+        True: имеет барометрическую высоту и скорость
+        False: иные случаи
+        '''
         return all((self.baro_altitude, self.velocity))
 
     def __getitem__(self, item: str | int | slice) -> Any:
+        '''
+        Получение атрибута объекта по индексу или имени атрибута.
+        Поддерживает int, str и срезы.
+        '''
         match item:
             case str():
                 if item in self.__slots__:
@@ -222,11 +291,17 @@ class Aeroplane(bc.BaseAeroplane):
 
     @classmethod
     def cast_to_aeroplane_gen(cls, aeroplanes) -> Generator:
+        '''
+        Преобразование списка словарей самолётов в объекты. Генератор.
+        '''
         for i in aeroplanes:
             yield cls(**i)
 
     @classmethod
     def cast_to_aeroplane(cls, aeroplanes) -> list:
+        '''
+        Преобразование списка словарей саамолётов в список объектов.
+        '''
         aeroplanes_ = []
         for i in aeroplanes:
             if i in aeroplanes_:
@@ -236,30 +311,55 @@ class Aeroplane(bc.BaseAeroplane):
         return [cls(**i) for i in aeroplanes_]
 
     def get_in_dict(self):
+        '''
+        Представление объекта в виде словаря атрибутов объекта и их значений
+        '''
         return {k: self.__getattribute__(k) for k in self.__slots__}
 
     @staticmethod
     def filter_by_country(countries: list[str], planes: list | tuple) -> list:
+        '''
+        Фильтр списка объектов по стране
+        '''
         return [i for i in planes if i.country in countries]
 
     def filter_predicate(self, countries: list):
+        '''
+        Функция-предикат для фильтрации по стране.
+        True: страна объекта находится в переданном в списке
+        False: иные случаи
+        '''
         return self.country in countries
 
     @staticmethod
     def get_top(data: list["Aeroplane"] | tuple["Aeroplane"], top_n: int = 5) -> list:
+        '''
+        Возвращает список top-n объектов по барометрической высоте.
+        '''
         return [i for i in
                 sorted(filter(lambda x: x.baro_altitude, data), key=lambda x: x.baro_altitude, reverse=True)][:top_n]
 
     @staticmethod
     def filter_by_range(data: list["Aeroplane"] | tuple["Aeroplane"], range_: tuple[int, int]) -> list:
+        '''
+        Возвращает список объектов в диапазоне высот от range_[0] до range_[1]
+        '''
         return [i for i in data if i.baro_altitude and range_[0] <= i.baro_altitude <= range_[1]]
 
     @staticmethod
     def filter_by_ground(data: list["Aeroplane"] | tuple["Aeroplane"], is_grounded: bool) -> list:
+        '''
+        Возвращает список объектов по нахождению на земле.
+        При is_grounded=True вернёт только самолёты на земле.
+        При is_grounded=False вернёт только самолёты в воздухе.
+        '''
         return [i for i in data if i.on_ground == is_grounded]
 
     @staticmethod
     def get_slice(data: list["Aeroplane"] | tuple["Aeroplane"], head: int = 3, tail: int = 2):
+        '''
+        Возвращает короткое представление списка объектов. Первые head объектов и последние tail самолётов
+        '''
         return data[:head].extend(data[tail:])
 
 
@@ -267,6 +367,9 @@ class JSONSaver(bc.BaseFile):
     __slots__ = ("__path", "data")
 
     def __init__(self, file_path: str, file_name: str = 'saved_aeroplanes'):
+        '''
+        Инициализатор объекта
+        '''
         self.__path = Path(file_path) / file_name
         if self.__path.suffix != ".json":
             self.__path = self.__path.with_suffix(".json")
@@ -274,6 +377,10 @@ class JSONSaver(bc.BaseFile):
         self.__create_path()
 
     def __getitem__(self, item: str) -> list:
+        '''
+        Возвращает список значений атрибута самолётов в data.
+        object_['Callsign'] вернёт list[str], где каждый элемент - значение атрибута Callsign каждого самолёта в data.
+        '''
         if isinstance(item, str):
             return [v for plane_data in self.data for k, v in plane_data.items() if k == item]
         else:
@@ -281,10 +388,16 @@ class JSONSaver(bc.BaseFile):
 
     @property
     def path(self):
+        '''
+        Возвращает путь до файла
+        '''
         return self.__path
 
 
     def __create_path(self) -> None:
+        '''
+        Создаёт путь до файла, если его не существует
+        '''
         if not self.__path.exists():
             self.__path.parent.mkdir(parents=True, exist_ok=True)
             self.__path.touch()
@@ -295,18 +408,30 @@ class JSONSaver(bc.BaseFile):
                 self.save_file()
 
     def __is_unique(self, aeroplane: dict) -> bool:
+        '''
+        Предикат для проверки уникальности самолёта. Приватный
+        True: ICAO24 самолёта нет data.
+        False: ICAO24 самолёта уже есть в data.
+        '''
         if aeroplane.get('ICAO24') in self['ICAO24']:
             return False
         else:
             return True
 
     def update_data(self, data: list) -> None:
+        '''
+        Переписывает данные на список словарей data, где data - список объектов Aeroplane
+        '''
         if isinstance(data, list):
             self.data = [i.get_in_dict() for i in data]
         else:
             raise TypeError(f"expected list. Got {type(data)}")
 
     def add_aeroplane(self, aeroplane) -> None:
+        '''
+        Добавляет один самолёт в данные, если тот уникален.
+        Иначе обновляет его данные на новые
+        '''
         aeroplane_dict = aeroplane.get_in_dict()
         if self.__is_unique(aeroplane_dict):
             self.data.append(aeroplane_dict)
@@ -315,22 +440,34 @@ class JSONSaver(bc.BaseFile):
                 self.data[self['ICAO24'].index(aeroplane_id)] = aeroplane_dict
 
     def delete_aeroplane(self, board_id) -> None:
+        '''
+        Удаляет самолёт из списка
+        '''
         for i in self.data:
             if i["ICAO24"] == board_id:
                 self.data.remove(i)
                 break
 
     def read_aeroplane(self) -> None:
+        '''
+        Читает файл и обновляет данные
+        '''
         self.__create_path()
         with open(self.__path, "r", encoding="utf-8") as file:
             self.data = json.load(file)
 
     def save_file(self) -> None:
+        '''
+        Сохраняет данные в файл
+        '''
         self.__create_path()
         with open(self.__path, "w", encoding="utf-8") as file:
             json.dump(self.data, file, indent=2, ensure_ascii=False)
 
     def get_fullpath(self):
+        '''
+        Возвращает полный путь
+        '''
         return Path.resolve(self.__path)
 
 
